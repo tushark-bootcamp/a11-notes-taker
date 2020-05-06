4a88a3f3ed8cef58290575a1a1fa8de1014b4d8c
TusharK <ktusharhk@gmail.com>
2020-05-07 09:39:24 +1000

    added code comments

--- data/notesData.js
+++ data/notesData.js
@@ -1,17 +1,19 @@
 var fs = require("fs");
 var newNote = require("./objects/note");
 
+// Reads and returns all the notes from db.json file
 var getNotesDB = () => {
-    var notesArrayStr = fs.readFileSync("./db/db.json", "utf8", function(error, data) {
-        if(error) {
+    var notesArrayStr = fs.readFileSync("./db/db.json", "utf8", function (error, data) {
+        if (error) {
             return console.log(error);
         }
         console.log("notesArray: " + data);
         return data;
     });
     return notesArrayStr;
-}; 
+};
 
+// Adds the new note to notesArray and saves to db.json file
 var saveNote = (note, notesArray) => {
     var uuid = uuidv4();
     console.log("logging note and newNote: ");
@@ -22,33 +24,36 @@ var saveNote = (note, notesArray) => {
     console.log(newNote);
     notesArray.push(newNote);
     createNoteDB(notesArray);
-  }
-  
- var createNoteDB = (notesArray) => {
+}
+
+// file function to add new note to db.json file
+var createNoteDB = (notesArray) => {
     var notes = JSON.stringify(notesArray);
     fs.writeFileSync("./db/db.json", notes, function (err) {
-      if (err) throw err;
-      console.log('Saved!');
+        if (err) throw err;
+        console.log('Saved!');
     });
-  }
+}
 
-  var deleteNote = (noteId, notesArray) => {
+// Deletes the note from the notesArray and deletes it from the db.json file
+var deleteNote = (noteId, notesArray) => {
     console.log("toDelete: " + noteId);
-    notesArray.forEach( (note, i) => {
-      //console.log("ith id: " + i + " " + note.id);
-      if(note.id === noteId) {
-        notesArray.splice(i, 1);
-      }
+    notesArray.forEach((note, i) => {
+        //console.log("ith id: " + i + " " + note.id);
+        if (note.id === noteId) {
+            notesArray.splice(i, 1);
+        }
     });
     createNoteDB(notesArray);
-  }
+}
 
-  var uuidv4 = () => {
+// generates unique id for the note
+var uuidv4 = () => {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
     });
-  }
+}
 
 module.exports = {
     getNotesDB: getNotesDB,
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// These data sources hold arrays of information on notes for reading notes, adding new notes and deleting notes.
 // ===============================================================================
 
 var notesData = require("../data/notesData");
